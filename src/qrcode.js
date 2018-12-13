d453a93d226df7f09d0e7ec021ebe2d6dcea492f
caoshuangna <qrgit02@163.com>
2018-12-13 14:12:37 +0800

    修复二维码中包含中文识别问题

--- src/qrcode.js
+++ src/qrcode.js
@@ -313,10 +313,43 @@ qrcode.process = function(ctx){
     var time = end - start;
     console.log(time);
     
-    return qrcode.decode_utf8(str);
-    //alert("Time:" + time + " Code: "+str);
+    // return qrcode.decode_utf8(str);
+    return qrcode.decode_url(str);
+    // var abcdefg = qrcode.decode_utf8(str); 
+    // return qrcode.utf8to16(abcdefg);
+    // alert("Time:" + time + " Code: "+str);
 }
-
+// qrcode.utf8to16 = function(str) {
+//     var out, i, len, c;
+//     var char2, char3;
+//     out = '';
+//     len = str.length;
+//     i = 0;
+//     while (i < len) {
+//     c = str.charCodeAt(i++);
+//     switch (c >> 4) {
+    
+//             case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+//             // 0xxxxxxx
+//             out += str.charAt(i - 1);
+//             break;
+//             case 12: case 13:
+//             // 110x xxxx   10xx xxxx
+//             char2 = str.charCodeAt(i++);
+//             out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+//             break;
+//             case 14:
+//                 // 1110 xxxx  10xx xxxx  10xx xxxx
+//                 char2 = str.charCodeAt(i++);
+//                 char3 = str.charCodeAt(i++);
+//                 out += String.fromCharCode(((c & 0x0F) << 12) |
+//                     ((char2 & 0x3F) << 6) |
+//                     ((char3 & 0x3F) << 0));
+//                 break;
+//         }
+//     }
+//     return out;
+// }
 qrcode.getPixel = function(x,y){
     if (qrcode.width < x) {
         throw "point error";
